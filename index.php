255ab1052c6c52f387a2e6d3778f82a6112dbbb6
AjiFattah <60567835+AjiFattah@users.noreply.github.com>
2020-05-17 16:34:34 +0700

    tubes

---
+++ index.php
@@ -0,0 +1,212 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <!-- Bootstrap CSS -->
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <title>193040022</title>
+  <style>
+
+    body {
+        background: salmon;
+    }
+    table {
+      border: #CCC solid 1px;
+      background: #f9f9f9;
+      box-shadow: 0 2px 7px rgba(0, 0, 0, 0.1);
+      border-radius: 20px;
+    }
+
+  </style>
+</head>
+<body>
+  <!-- navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container">
+      <a class="navbar-brand" href="#">193040012</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item">
+            <a class="nav-item nav-link" href="#home"><b>Home</b> <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#profile"><b>Profile</b></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#kuliah"><b>Kuliah</b></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#praktikum"><b>Praktikum</b></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#tubes"><b>Tugas Besar</b></a>
+          </li>
+        </ul>
+        <form class="form-inline my-2 my-lg-0">
+          <ul class="nav nav-pills card-header-pills pl-3">
+            <li class="nav-item">
+              <a class="nav-link active">Sign up</a>
+            </li>
+            <li class="nav-item pl-2">
+              <a class="nav-link text-dark bg-light">Log in</a>
+            </li>
+          </ul>
+        </form>
+      </div>
+    </div>
+  </nav>
+
+  <!-- home -->
+  <section id="home" class="home">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-5 text-center">
+          <br><br><br><br><br>
+          <h1 class="display-4" style="text-align: left; font-size: 60px;"><b>Welcome</b></h1>
+          <br>
+          <p style=" text-align: left; font-size: 20px;">ini adalahn index menuju <b>Profile</b>, <b>Tugas Kuliah</b>, <b>Tugas Praktikum</b>, dan <b>Tugas Besar</b></p>
+          <br><br><br><br><br><br><br><br><br><br><br><br>
+        </div>
+        <div class="col-md-5 text-center">
+          <img src="assets/img/kuliah.png" width=100%">
+        </div>
+      </div>
+    </div>
+  </section>
+
+    <!-- profile -->
+    <section id="profile" class="profile">
+    <div class="container">
+      <div class="row justify-content-center">
+        <div class="col-md-5 text-center">
+          <br><br><br>
+          <img src="assets/img/user.jpg" width=50%" class="rounded-box img-thumbnail">
+          <br><br><br>
+        </div>
+        <div class="col-md-5 text-center">
+          <br><br><br>
+          <h2 style="text-align: left; font-size: 38px;">Aji fattah adriansyah darajat</h2>
+          <br>
+          <p style="text-align: left; font-size: 18px;">Saya adalah mahasiswa jurusan <b>Teknik Informatika</b> yang sedang menempuh pendidikan strata 1 di <b>Universitas Pasundan</b> saya mengerjakan tugas ini untuk memenuhi nilai dari mata kuliah pemrograman web</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- kuliah -->
+  <section id="kuliah" class="kuliah" style="background-color: Gainsboro;">
+    <div class=" container">
+      <div class="row mb-4">
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-5 text-center">
+          <table class="table">
+            <thead class="thead-dark">
+              <br>
+              <h2 style="font-size: 38px;">Tugas Kuliah</h2>
+              <br>
+              <p style="text-align: center; font-size: 20px;"><b>Username:</b> admin <b>Password:</b> 12345</p>
+              <br>
+              <tr style="font-size: 20px;">
+                <th scope="col">Repositories</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr style="font-size: 20px;">
+                <td><a href="kuliah/pertemuan 9">Pertemuan 9</a></td>
+              </tr>
+              <tr style="font-size: 20px;">
+                <td><a href="kuliah/pertemuan10">Pertemuan 10</td>
+              </tr>
+              <tr style="font-size: 20px;">
+                <td><a href="kuliah/pertemuan11">Pertemuan 11</td>
+              </tr>
+              <tr style="font-size: 20px;">
+                <td><a href="kuliah/pertemuan12">Pertemuan 12</td>
+              </tr>
+              <tr style="font-size: 20px;">
+                <td><a href="kuliah/pertemuan13">Pertemuan 13</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+    
+      </div>
+    </div>
+  </section>
+  <!-- praktikum -->
+  <section id="praktikum" class="praktikum">
+    <div class=" container">
+      <div class="row mb-4">
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-5 text-center">
+          <table class="table">
+            <thead class="thead-dark">
+              <br>
+              <h2 style="font-size: 38px;">Tugas Praktikum</h2>
+              <br>
+              <p style="text-align: center; font-size: 20px;"><b>Username:</b> admin <b>Password:</b> admin</p>
+              <br>
+              <tr style="font-size: 20px;">
+                <th scope="col">Repositories</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr style="font-size: 20px;">
+                <td><a href="praktikum/P5_PW_193040012">P5_SENIN13_193040012</td>
+              </tr>
+              <tr style="font-size: 20px;">
+                <td><a href="praktikum/P6_PW_193040012">P6_SENIN13_193040012</td>
+              </tr>
+              <tr style="font-size: 20px;">
+                <td><a href="praktikum/P7_PW_193040012">P7_SENIN13_193040012</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+     
+      </div>
+    </div>
+  </section>
+  <!-- tubes -->
+  <section id="tubes" class="tubes" style="background-color: Gainsboro;">
+    <div class=" container">
+      <div class="row mb-4">
+      </div>
+      <div class="row justify-content-center">
+        <div class="col-md-5 text-center">
+          <table class="table">
+            <thead class="thead-dark">
+              <br>
+              <h2 style="font-size: 38px;">Tugas Besar</h2>
+              <br>
+              <p style="text-align: left; font-size: 20px;"><b>Username:</b> admin <b>Password:</b> admin</p>
+              <tr style="font-size: 20px;">
+                <th scope=" col">Repositories</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr style="font-size: 20px;">
+                <td><a href="tubes">Tugas Besar</a></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+  
+      </div>
+    </div>
+  </section>
+  <!-- Optional JavaScript -->
+  <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+</html>
